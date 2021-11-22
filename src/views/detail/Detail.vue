6aae756cdbc7f78fdd092e6f484deb809b057095
Conrad4 <15344099553@qq.com>
2021-11-22 23:45:33 +0800

    feat: add DetailShopInfo,imgLoad listening optimization

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="detail">
-    <!-- <nav-bar></nav-bar> -->
+  <div id="detail">
     <detail-nav-bar @titleClick="titleClick"></detail-nav-bar>
-    <detail-swiper :banners="topImages"></detail-swiper>
-    <detail-base-info :goods="goods" />
+    <scroll class="content">
+      <detail-swiper :banners="topImages"></detail-swiper>
+      <detail-base-info :goods="goods" />
+    </scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar.vue";
-import DetailBaseInfo from "./childComps/DetailBaseInfo";
+import DetailBaseInfo from "./childComps/DetailBaseInfo.vue";
 import DetailSwiper from "./childComps/DetailSwiper.vue";
+
+import Scroll from "components/common/scroll/Scroll.vue";
 
 import { getGoodsDetail, Goods } from "network/detail";
 
 export default {
   name: "Detail",
-  components: { NavBar, DetailNavBar, DetailSwiper, DetailBaseInfo},
+  components: { NavBar, DetailNavBar, DetailSwiper, DetailBaseInfo, Scroll },
   props: {},
   data() {
     return {
@@ -24,6 +27,7 @@ export default {
       topImages: [],
       // 把这个值传到DetailbaseInfo子组件,这个是空对象，不要写成null或者其他
       goods: {},
+      shop: {},
     };
   },
   created() {
@@ -33,6 +37,7 @@ export default {
     getGoodsDetail(this.iid).then((res) => {
       // 后端拿到的数据是data.result,先把公共的部分定义出来，就防止出现res.data.result.itemInfo.xxx,太长了，这和后端返回的数据有关系
       const data = res.data.result;
+      console.log(data);
       this.topImages = data.itemInfo.topImages;
 
       this.goods = new Goods(
@@ -40,6 +45,15 @@ export default {
         data.columns,
         data.shopInfo.services
       );
+      // 使用解构写一下;shopInfo.services这种就不行了
+      // const { itemInfo, columns } = data;
+      // this.goods = new Goods(itemInfo, columns, data.shopInfo.services);
+
+      this.shop = new Shop(data.shopInfo);
+
+      // 如果下面不是这样写的话， 你在子组件里面prop传值的时候，就得穿logo，name等等，传的值太多了，所以封装一下还是有必要的，
+      // this.logo = data.shopInfo.shopLogo;
+      // this.name = data.shopInfo.name;
     });
   },
   mounted() {},
@@ -48,4 +62,14 @@ export default {
   methods: {},
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.content {
+  height: calc(100% - 44px);
+}
+</style>
